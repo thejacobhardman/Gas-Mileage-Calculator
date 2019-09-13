eccc24c0f88931177e9ac9b5f029edc328470fb9
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-13 16:24:17 -0600

    add basic framework

--- Gas-Mileage-Calculator/Program.cs
+++ Gas-Mileage-Calculator/Program.cs
@@ -10,7 +10,46 @@ namespace Gas_Mileage_Calculator
     {
         static void Main(string[] args)
         {
+            Boolean isRunning = true;
+            double totalMPG = 0.00;
+            double newMPG;
+            double miles;
+            double gallons;
+            //double[] MPGArr = new double[0];
+            List<double> MPGList = new List<double>(); 
 
+            while (isRunning)
+            {
+                Console.Write("Enter the number of miles driven (-1 to quit): ");
+                miles = Convert.ToDouble(Console.ReadLine());
+                if (miles == -1)
+                {
+                    isRunning = false;
+                    break;
+                }
+                Console.Write("Enter the number of gallons used: ");
+                gallons = Convert.ToDouble(Console.ReadLine());
+
+                newMPG = miles / gallons;
+                MPGList.Add(newMPG);
+
+                for (int i = 0; i < MPGList.Count; i++) {
+                    totalMPG += MPGList[i];
+                }
+
+                totalMPG /= MPGList.Count;
+
+                Console.WriteLine(MPGList);
+                Console.WriteLine(MPGList.Count);
+                Console.ReadLine();
+
+                Console.WriteLine("MPG this tankful: " + MPGList[MPGList.Count]);
+                Console.WriteLine("Total MPG: " + totalMPG);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press 'enter' to close the program.");
+            Console.ReadLine();
         }
     }
 }
